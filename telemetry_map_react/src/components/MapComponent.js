be1a60681c7213b8e6680422315bce51ea64e8fc
blainefreestone <blainefreestone01@gmail.com>
2024-02-23 23:27:06 -0700

    mapcomponent can now add point to graphiclayer

--- telemetry_map_react/src/components/MapComponent.js
+++ telemetry_map_react/src/components/MapComponent.js
@@ -1,14 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Config from '@arcgis/core/config';
 import Map from '@arcgis/core/Map';
 import SceneView from '@arcgis/core/views/SceneView';
 import Graphic from '@arcgis/core/Graphic';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
 
+// component to get and update map div
 class MapComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.mapDiv = React.createRef();
+    this.mapDiv = React.createRef();  // create a reference to the map div
+    this.state = {
+      points: [], // array to hold the points
+    }
+  }
+
+  // add a new point to the state
+  addPoint = (point) => {
+    this.setState(prevState => ({
+      points: [...prevState.points, point], // add the new point to the array
+    }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.points !== this.state.points) {
+      const point = this.state.points[this.state.points.length - 1]; // get the last point (i.e., the new point)
+
+      // Create a graphic for the new point
+      const pointGraphic = new Graphic({
+        geometry: point,
+        symbol: this.markerSymbol,
+        outFields: ["*"],
+        attributes: {
+          x: point.x,
+          y: point.y,
+          z: Math.round(point.z)
+        },
+        popupTemplate: this.pointPopupTemplate,
+      })
+
+      // Add the new point to the graphics layer
+      this.graphicsLayer.add(pointGraphic);
+    }
   }
 
   componentDidMount() {
@@ -16,15 +49,15 @@ class MapComponent extends React.Component {
       Config.apiKey = "AAPK18b2e18170204bc6b4d16bee66e69afdkvVIvyYk4T2gtPkbxLqIWRX7Zxgpo_8fQYZ60kmRrsKvTtVNjto-_jzQ6UuD2Jy3";
 
       // Create the Map
-      const map = new Map({
+      this.map = new Map({
         basemap: "arcgis/topographic",  // basemap styles service
         ground: "world-elevation"       // elevation styles service
       });
 
       // Create the SceneView
-      const view = new SceneView({
+      this.view = new SceneView({
         container: this.mapDiv.current,   // HTML element to place the map
-        map: map,
+        map: this.map,
         // starting position of the camera
         camera: {
           position: [
@@ -36,12 +69,33 @@ class MapComponent extends React.Component {
           heading: 360
         }
       });
+
+      // Create a graphics layer to hold the points and lines
+      this.graphicsLayer = new GraphicsLayer();
+      this.map.add(this.graphicsLayer);
+
+      // Create a symbol with style for the points
+      this.markerSymbol = {
+        type: "simple-marker",
+        color: [226, 119, 40], // Orange
+        outline: {
+          color: [200, 200, 200], // Grey
+          width: .75
+        },
+        size: 5 // Adjust the size value to make the symbol smaller
+      };
+
+      // Create a popup template for the points
+      this.pointPopupTemplate = {
+      title: "Information",
+      content: "Latitude: {x} <br> Longitude: {y} <br> Altitude: {z} meters"
+    };
     }
   }
 
   render() {
     return (
-      <div ref={this.mapDiv} style={{ width: '100%', height: '100vh' }}></div>
+      <div ref={this.mapDiv} style={{ width: '90%', height: '90vh' }}></div>
     );
   }
 }
