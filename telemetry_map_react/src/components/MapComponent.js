a50077ebebb83ff9b9c08e4004d96de416446068
blainefreestone <blainefreestone01@gmail.com>
2024-02-24 09:43:36 -0700

    refactored map component to clear and recreate graphics layer on each update

--- telemetry_map_react/src/components/MapComponent.js
+++ telemetry_map_react/src/components/MapComponent.js
@@ -4,11 +4,13 @@ import Map from '@arcgis/core/Map';
 import SceneView from '@arcgis/core/views/SceneView';
 import Graphic from '@arcgis/core/Graphic';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import AddPoint from './AddPoint';
 
 // component to get and update map div
 class MapComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.addPoint = this.addPoint.bind(this);
     this.mapDiv = React.createRef();  // create a reference to the map div
     this.state = {
       points: [], // array to hold the points
@@ -16,31 +18,42 @@ class MapComponent extends React.Component {
   }
 
   // add a new point to the state
-  addPoint = (point) => {
+  addPoint = (pointValues) => {
+    // convert the point values into a point object
+    const point = {
+      type: "point",
+      x: pointValues.x,
+      y: pointValues.y,
+      z: pointValues.z
+    }
+
+    // Create a graphic for the new point
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: this.markerSymbol,
+      outFields: ["*"],
+      attributes: {
+        x: point.x,
+        y: point.y,
+        z: Math.round(point.z)
+      },
+      popupTemplate: this.pointPopupTemplate,
+    })
+
     this.setState(prevState => ({
-      points: [...prevState.points, point], // add the new point to the array
+      points: [...prevState.points, pointGraphic], // add the new point to the array
     }));
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.points !== this.state.points) {
-      const point = this.state.points[this.state.points.length - 1]; // get the last point (i.e., the new point)
+      // clear the graphics layer
+      this.graphicsLayer.removeAll();
 
-      // Create a graphic for the new point
-      const pointGraphic = new Graphic({
-        geometry: point,
-        symbol: this.markerSymbol,
-        outFields: ["*"],
-        attributes: {
-          x: point.x,
-          y: point.y,
-          z: Math.round(point.z)
-        },
-        popupTemplate: this.pointPopupTemplate,
-      })
-
-      // Add the new point to the graphics layer
-      this.graphicsLayer.add(pointGraphic);
+      // add the points to the graphics layer
+      this.state.points.forEach((point) => {
+        this.graphicsLayer.add(point);
+      });
     }
   }
 
@@ -95,7 +108,10 @@ class MapComponent extends React.Component {
 
   render() {
     return (
-      <div ref={this.mapDiv} style={{ width: '90%', height: '90vh' }}></div>
+      <>
+        <div ref={this.mapDiv} style={{ width: '90%', height: '90vh' }}></div>
+        <AddPoint  addPoint={this.addPoint}/>
+      </>
     );
   }
 }
