0d8d9feaeb5335dd2e0b09a11eb325ddbb4ed79e
blainefreestone <blainefreestone01@gmail.com>
2024-02-28 15:26:28 -0700

    functions to read and write data from files added

---
+++ telemetry_map_server/dataHandler.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// directory to save data files
+const saveDirectory = './data';
+
+// save data into file as JSON
+const saveDataToFile = (data, fileName) => {
+    // create filepath from saveDirectory and fileName
+    const filePath = path.join(saveDirectory, fileName);
+    // write data to file in JSON format
+    fs.writeFileSync(filePath, JSON.stringify(data));
+}
+
+// load data in JSON format from file
+const loadDataFromFile = (filename) => {
+    // create filepath from saveDirectory and filename
+    const filePath = path.join(saveDirectory, filename);
+    // return data from file
+    return JSON.parse(fs.readFileSync(filePath));
+}
+
+export default { saveDataToFile, loadDataFromFile };
