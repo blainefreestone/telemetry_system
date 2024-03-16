09cafcde97a7fc6e34501bf531791bfc91010f25
blainefreestone <blainefreestone01@gmail.com>
2024-03-16 16:09:57 -0600

    reads GPS data, parses, and sends latitude, longitude, and altitude to the server

--- telemetry_circuit_sketch/telemetry_circuit_sketch.ino
+++ telemetry_circuit_sketch/telemetry_circuit_sketch.ino
@@ -1,69 +1,75 @@
 #include "MPU9250.h"
-#include "MS5611.h"
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 MPU9250 mpu;
-MS5611 MS5611(0x77);
-float quatX;
+float quatX;      // hold quaternion values that will be changed to roll, pitch, and yaw
 float quatY;
 float quatZ;
 float quatW;
-float pressure;
-float temp;
+float roll;       // hold roll pitch and yaw values
+float pitch;
+float yaw;
+float temp;       // holds temperature
+float latitude;   // hold GPS location data
+float longitude;
+unsigned long locationAge;
+float altitude;
+
+int RXPin = 2;
+int TXPin = 3;
+
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+TinyGPS gps;
 
 void setup() {
+    // start serial usb connection
     Serial.begin(115200);
+    // start i2c connection
     Wire.begin();
+    // start gps serial connection
+    gpsSerial.begin(9600);
+
     delay(2000);
 
     // configure I2C address for IMU
     mpu.setup(0x68);
     // toggle automatic cleaning up of data
     mpu.ahrs(true);
-
-    // MS5611.init();
-    // configure pressure sensor
-    if (MS5611.begin()) {
-      Serial.print("MS5611 Found");
-    }
-    else {
-      Serial.print("Not found");
-    }
-    delay(5000);
-
-    MS5611.reset(1);
-    // set sampling rate to ultra low for highest accuracy
-    MS5611.setOversampling(OSR_ULTRA_LOW);
 }
 
 void loop() {
     // get orientation data from MPU
-    if (mpu.update()) {
-        quatX = mpu.getQuaternionX();
-        quatY = mpu.getQuaternionY();
-        quatZ = mpu.getQuaternionZ();
-        quatW = mpu.getQuaternionW();
-        quat2euler(quatX, quatY, quatZ, quatW);
+    if ((mpu.update()) && (gpsSerial.available() > 0)) {
+      // get and calculate data from IMU
+      quatX = mpu.getQuaternionX();
+      quatY = mpu.getQuaternionY();
+      quatZ = mpu.getQuaternionZ();
+      quatW = mpu.getQuaternionW();
+      quat2euler(quatX, quatY, quatZ, quatW, roll, pitch, yaw);
+
+      // get and parse data from GPS
+      gps.encode(gpsSerial.read());   // get NMEA sentence and parse it
+      gps.f_get_position(&latitude, &longitude, &locationAge);  // get position data and data age
+      altitude = gps.f_altitude();  // get altitude
+
+      Serial.print(roll);
+      Serial.print(",");
+      Serial.print(pitch);
+      Serial.print(",");
+      Serial.print(yaw);
+      Serial.print(",");
+      Serial.print(latitude);
+      Serial.print(",");
+      Serial.print(longitude);
+      Serial.print(",");
+      Serial.println(altitude);
     }
-    baro_data();
 }
 
-void quat2euler(float q0, float q1, float q2, float q3) {
-    float roll, pitch, yaw;
+void quat2euler(float q0, float q1, float q2, float q3, float &roll, float &pitch, float &yaw) {
     roll = atan2(2*(q0*q1 + q2*q3), 1 - 2*(q1*q1 + q2*q2));
     pitch = asin(2*(q0*q2 - q3*q1));
     yaw = atan2(2*(q0*q3 + q1*q2), 1 - 2*(q2*q2 + q3*q3));
-    Serial.print(roll);
-    Serial.print(",");
-    Serial.print(pitch);
-    Serial.print(",");
-    Serial.println(yaw);
-}
-
-void baro_data() {
-    // get pressure data from baro
-    pressure = MS5611.getPressure();
-    temp = MS5611.getTemperature();
-    Serial.print(pressure);
-    Serial.print(",");
-    Serial.println(temp);
 }
